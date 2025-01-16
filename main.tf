44b0e009fcc7373a23c10cfcb8306cc7976f9723
Arunsai14 <arun.sai@sourcefuse.com>
2025-01-16 10:37:10 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -145,81 +145,81 @@ resource "aws_lb_target_group" "this" {
 ## Listener
 ###################################################################
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = var.alb.port
-  protocol          = var.alb.protocol
+# resource "aws_lb_listener" "http" {
+#   load_balancer_arn = aws_lb.this.arn
+#   port              = var.alb.port
+#   protocol          = var.alb.protocol
 
-  certificate_arn = var.alb.certificate_arn
+#   certificate_arn = var.alb.certificate_arn
 
-  # Static "default_action" for forward
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.this[var.alb_target_group[0].name].arn
-  }
+#   # Static "default_action" for forward
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.this[var.alb_target_group[0].name].arn
+#   }
 
-  # Dynamic "default_action" for variable-driven actions
-  dynamic "default_action" {
-    for_each = var.listener_rules
+#   # Dynamic "default_action" for variable-driven actions
+#   dynamic "default_action" {
+#     for_each = var.listener_rules
 
-    content {
-      type             = length(each.value.actions) > 0 ? each.value.actions[0].type : null
-      target_group_arn = length(each.value.actions) > 0 ? lookup(each.value.actions[0], "target_group_arn", null) : null
-    }
-  }
-  depends_on = [aws_lb_target_group.this]
-}
+#     content {
+#       type             = length(each.value.actions) > 0 ? each.value.actions[0].type : null
+#       target_group_arn = length(each.value.actions) > 0 ? lookup(each.value.actions[0], "target_group_arn", null) : null
+#     }
+#   }
+#   depends_on = [aws_lb_target_group.this]
+# }
 
 
 ###################################################################
 ## Listener Rules
 ###################################################################
-resource "aws_lb_listener_rule" "this" {
-  for_each = var.create_listener_rule ? { for rule in var.listener_rules : rule.priority => rule } : {}
+# resource "aws_lb_listener_rule" "this" {
+#   for_each = var.create_listener_rule ? { for rule in var.listener_rules : rule.priority => rule } : {}
 
-  listener_arn = aws_lb_listener.http.arn
-  priority     = each.value.priority
+#   listener_arn = aws_lb_listener.http.arn
+#   priority     = each.value.priority
 
-  dynamic "condition" {
-    for_each = each.value.conditions
-    content {
-      dynamic "host_header" {
-        for_each = each.value.field == "host-header" ? [each.value] : []
-        content {
-          values = each.value.values
-        }
-      }
+#   dynamic "condition" {
+#     for_each = each.value.conditions
+#     content {
+#       dynamic "host_header" {
+#         for_each = each.value.field == "host-header" ? [each.value] : []
+#         content {
+#           values = each.value.values
+#         }
+#       }
 
-      dynamic "path_pattern" {
-        for_each = each.value.field == "path-pattern" ? [each.value] : []
-        content {
-          values = each.value.values
-        }
-      }
-    }
-  }
+#       dynamic "path_pattern" {
+#         for_each = each.value.field == "path-pattern" ? [each.value] : []
+#         content {
+#           values = each.value.values
+#         }
+#       }
+#     }
+#   }
 
-  dynamic "action" {
-    for_each = each.value.actions
-    content {
-      type             = action.value.type
-      target_group_arn = lookup(action.value, "target_group_arn", aws_lb_target_group.this.arn)
-      order            = lookup(action.value, "order", null)
-      redirect {
-        protocol    = lookup(action.value.redirect, "protocol", null)
-        port        = lookup(action.value.redirect, "port", null)
-        host        = lookup(action.value.redirect, "host", null)
-        path        = lookup(action.value.redirect, "path", null)
-        query       = lookup(action.value.redirect, "query", null)
-        status_code = lookup(action.value.redirect, "status_code", null)
-      }
-      fixed_response {
-        content_type = lookup(action.value.fixed_response, "content_type", null)
-        message_body = lookup(action.value.fixed_response, "message_body", null)
-        status_code  = lookup(action.value.fixed_response, "status_code", null)
-      }
-    }
-  }
+#   dynamic "action" {
+#     for_each = each.value.actions
+#     content {
+#       type             = action.value.type
+#       target_group_arn = lookup(action.value, "target_group_arn", aws_lb_target_group.this.arn)
+#       order            = lookup(action.value, "order", null)
+#       redirect {
+#         protocol    = lookup(action.value.redirect, "protocol", null)
+#         port        = lookup(action.value.redirect, "port", null)
+#         host        = lookup(action.value.redirect, "host", null)
+#         path        = lookup(action.value.redirect, "path", null)
+#         query       = lookup(action.value.redirect, "query", null)
+#         status_code = lookup(action.value.redirect, "status_code", null)
+#       }
+#       fixed_response {
+#         content_type = lookup(action.value.fixed_response, "content_type", null)
+#         message_body = lookup(action.value.fixed_response, "message_body", null)
+#         status_code  = lookup(action.value.fixed_response, "status_code", null)
+#       }
+#     }
+#   }
 
-  depends_on = [aws_lb_listener.http]
-}
+#   depends_on = [aws_lb_listener.http]
+# }
