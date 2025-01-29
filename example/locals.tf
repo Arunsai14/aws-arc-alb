54610ee84d774a8b6432586579c6c08a94f22782
Arunsai14 <arun.sai@sourcefuse.com>
2025-01-29 22:21:03 +0530

    initial commit

--- example/locals.tf
+++ example/locals.tf
@@ -24,10 +24,10 @@ load_balancer_config = {
 
   subnet_mapping = [
     {
-      subnet_id            = [for s in data.aws_subnet.private : s.id]
+      subnet_id            = data.aws_subnet.private.id[0]
     },
     {
-      subnet_id            = "data.aws_subnet.private.id"
+      subnet_id            = data.aws_subnet.private.id[1]
     }
   ]
 
