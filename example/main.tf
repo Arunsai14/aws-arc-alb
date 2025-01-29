e1c6ad89001167c430ea39ae85acc9eb936d68d8
Arunsai14 <arun.sai@sourcefuse.com>
2025-01-29 11:00:15 +0530

    initial commit

--- example/main.tf
+++ example/main.tf
@@ -36,7 +36,7 @@ module "alb" {
   target_group_config            = var.target_group_config
   target_group_attachment_config = var.target_group_attachment_config
   alb_listener            = var.alb_listener
-  #  default_action          = var.default_action
+  default_action          = var.default_action
   # listener_rules          = var.listener_rules
   security_group_data     = var.security_group_data
   security_group_name     = var.security_group_name
